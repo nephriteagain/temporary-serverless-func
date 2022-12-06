ee725fd6ee93ec97baca1e2773373d1e17a5685c
nephriteagain <110908855+nephriteagain@users.noreply.github.com>
2022-12-06 21:39:08 +0800

    bug fix

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -4,14 +4,10 @@ const items = require('../assets/data')
 exports.handler = async ( event, context ) => {
     console.log(event)
     return {
-        // this fix CORS error
         headers: {
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
         },
-        // 
         statusCode: 200,
-        // body: JSON.stringify(person)
-        // body: 'Our Basic API Example'
         body: JSON.stringify(items)
     }
 }
