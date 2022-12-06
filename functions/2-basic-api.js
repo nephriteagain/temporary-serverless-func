95f2ceb958c5cd0d37135f6fc2de06476d9ea326
nephriteagain <110908855+nephriteagain@users.noreply.github.com>
2022-12-06 21:21:42 +0800

    CORS error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -4,6 +4,9 @@ const items = require('../assets/data')
 exports.handler = async ( event, context ) => {
     console.log(event)
     return {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
         statusCode: 200,
         // body: JSON.stringify(person)
         // body: 'Our Basic API Example'
