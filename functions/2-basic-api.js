758199dcb90f167d217c98dce22845da4a32c51a
nephriteagain <110908855+nephriteagain@users.noreply.github.com>
2022-12-06 21:29:14 +0800

    returned basic-api folder

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -4,9 +4,11 @@ const items = require('../assets/data')
 exports.handler = async ( event, context ) => {
     console.log(event)
     return {
+        // this fix CORS error
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
+        // 
         statusCode: 200,
         // body: JSON.stringify(person)
         // body: 'Our Basic API Example'
